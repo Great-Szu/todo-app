745157c789c699152f52e7db9952c1d06b981385
Szu <mateusz.chol.net@gmail.com>
2024-06-27 19:10:58 +0200

    Fixing Errors day 17

--- gui.py
+++ gui.py
@@ -1,6 +1,10 @@
+import time
 import functions
 import FreeSimpleGUI as sg
 
+sg.theme("SystemDefault")
+
+clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
@@ -14,6 +18,7 @@ exit_button = sg.Button("Exit")
 
 window = sg.Window('My To-Do App',
                    layout=[
+                       [clock],
                        [label],
                        [input_box, add_button],
                        [list_box, edit_button, complete_button],
@@ -23,43 +28,50 @@ window = sg.Window('My To-Do App',
                    )
 
 while True:
-    event, values = window.read()
-    print(event)
-    print(values)
-    match event:
-        case "Add":
-            todos = functions.get_todos()
-            new_todo = values['todo'] + "\n"
-            todos.append(new_todo)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(value='')
+    event, values = window.read(timeout=200)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+    try:
+        match event:
+            case "Add":
+                todos = functions.get_todos()
+                new_todo = values['todo'] + "\n"
+                todos.append(new_todo)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
 
-        case "Edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
+            case "Edit":
+                try:
+                    todo_to_edit = values['todos'][0]
+                    new_todo = values['todo']
 
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
+                    todos = functions.get_todos()
+                    index = todos.index(todo_to_edit)
+                    todos[index] = new_todo
+                    functions.write_todos(todos)
+                    window['todos'].update(values=todos)
+                except IndexError:
+                    sg.popup("Please select todo")
 
-        case "Complete":
-            todo_to_complete = values['todos'][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(value='')
+            case "Complete":
+                try:
+                    todo_to_complete = values['todos'][0]
+                    todos = functions.get_todos()
+                    todos.remove(todo_to_complete)
+                    functions.write_todos(todos)
+                    window['todos'].update(values=todos)
+                    window['todo'].update(value='')
+                except IndexError:
+                    sg.popup("Please select todo")
 
-        case "Exit":
-            break
+            case "Exit":
+                break
 
-        case 'todos':
-            window['todo'].update(value=values['todos'][0])
+            case 'todos':
+                window['todo'].update(value=values['todos'][0])
 
-        case sg.WIN_CLOSED:
-            break
-
+            case sg.WIN_CLOSED:
+                break
+    except IndexError:
+        sg.popup("The list is empty")
 window.close()
